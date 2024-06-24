a473957435fec08e6bf5251c1e38b8689916febf
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-24 14:17:59 +0530

    create navbar design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { NavLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+import ProfileMenu from './ProfileMenu'
+import Button from './Button'
+import AuthProviders from './AuthProviders'
 
 const Navbar = () => {
+  const session = {}
+
   return (
-    <div>Navbar</div>
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            width={116}
+            height={43}
+            alt='logo'
+          />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className='flexCenter gap-4'>
+        {session ? (
+          <>
+            <ProfileMenu />
+            <Link href="/create-project">
+              <Button />
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
   )
 }
 
